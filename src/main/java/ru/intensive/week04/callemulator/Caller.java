64611b9d6244732eb7345ac9db9a61f3dee5ea26
BBergsJ <goldlike888@gmail.com>
2023-05-20 12:29:46 +0300

    Week 4: Add formatter

--- src/main/java/ru/intensive/week04/callemulator/Caller.java
+++ src/main/java/ru/intensive/week04/callemulator/Caller.java
@@ -5,7 +5,6 @@ import java.util.Date;
 public class Caller implements CallService{
     @Override
     public void printMessage() {
-        String formattedTime = dateFormat.format(new Date(System.currentTimeMillis()));
-        System.out.printf("Print to Terminal! Current time is : %s%n", formattedTime);
+        System.out.printf("Print to Terminal! Current time is : %s%n", doFormat(System.currentTimeMillis()));
     }
 }
